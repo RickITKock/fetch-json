84c9c507d1b16485a2a334a492249f86f57ad03c
Rick Kock <rit.kock@gmail.com>
2022-08-30 21:15:56 +0200

    Add simple annotation examples

---
+++ annotations/variables.ts
@@ -0,0 +1,10 @@
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+let colors: string[] = ["red", "green", "blue"];
+
+class Car {}
+
+let car: Car = new Car();
+
+let point: { x: number; y: number } = { x: 10, y: 20 };
